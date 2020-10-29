1e7eede0bfebe55ac4b01276a39fcf5945830fea
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-10-29 08:19:32 +0100

    create tables then populate

--- db.js
+++ db.js
@@ -2,7 +2,16 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 const settings = require('./settings');
 
-const pool = mysql.createConnection(settings.mysql);
+let mysqlSettings = settings.mysql;
+if (
+  process.env.NODE_ENV === 'production' &&
+  typeof mysqlSettings === 'string'
+) {
+  console.log('using prod env');
+  mysqlSettings += '&multipleStatements=true';
+}
+
+const pool = mysql.createConnection(mysqlSettings);
 const query = promisify(pool.query.bind(pool));
 
 module.exports = {
--- tools/populate.js
+++ tools/populate.js
@@ -1,7 +1,13 @@
 const Promise = require('bluebird');
 require('dotenv').config();
+const path = require('path');
+const fs = require('fs');
+const { create } = require('lodash');
 const { posts } = require('../db.json');
 const db = require('../db');
+
+const schemaPath = path.resolve(__dirname, '../schema.sql');
+const schemaSql = fs.readFileSync(schemaPath, 'utf8');
 
 const getPicture = ({ medium }) => medium.split('/').pop();
 
@@ -10,47 +16,59 @@ const getTagRecord = async (tagName) =>
     .query('SELECT * FROM `tag` WHERE name = ?', tagName)
     .then((records) => (records.length ? records[0] : null));
 
-Promise.reduce(
-  posts,
-  async (carry, { title, picture, tags, country, date }) => {
-    const payload = {
-      title,
-      picture: getPicture(picture),
-      country,
-      date,
-      authorId: 1,
-    };
-    // console.log(payload);
-    const { insertId: photoId } = await db.query(
-      'INSERT INTO photo SET ?',
-      payload,
-    );
-    // console.log('inserted photo with id', resultId);
-    const tagIds = [];
-    await Promise.reduce(
-      tags,
-      async (carry, tagName) => {
-        const tagRecord = await getTagRecord(tagName);
-        if (tagRecord) {
-          tagIds.push(tagRecord.id);
-          return false;
-        }
-        const { insertId } = await db.query('INSERT INTO tag SET ?', {
-          name: tagName,
-        });
-        tagIds.push(insertId);
-        return true;
-      },
-      [],
-    ).catch((err) => {
-      console.error('ERR', err);
-      throw err;
-    });
-    await Promise.map(tagIds, (tagId) =>
-      db.query('INSERT INTO photo_tag SET ?', { photoId, tagId }),
-    ).catch((err) => {
-      console.error('ERR', err);
-      throw err;
-    });
-  },
-).then(() => process.exit());
+const populatePhotos = async () =>
+  Promise.reduce(
+    posts,
+    async (carry, { title, picture, tags, country, date }) => {
+      const payload = {
+        title,
+        picture: getPicture(picture),
+        country,
+        date,
+        authorId: 1,
+      };
+      // console.log(payload);
+      const { insertId: photoId } = await db.query(
+        'INSERT INTO photo SET ?',
+        payload,
+      );
+      // console.log('inserted photo with id', resultId);
+      const tagIds = [];
+      await Promise.reduce(
+        tags,
+        async (carry, tagName) => {
+          const tagRecord = await getTagRecord(tagName);
+          if (tagRecord) {
+            tagIds.push(tagRecord.id);
+            return false;
+          }
+          const { insertId } = await db.query('INSERT INTO tag SET ?', {
+            name: tagName,
+          });
+          tagIds.push(insertId);
+          return true;
+        },
+        [],
+      ).catch((err) => {
+        console.error('ERR', err);
+        throw err;
+      });
+      await Promise.map(tagIds, (tagId) =>
+        db.query('INSERT INTO photo_tag SET ?', { photoId, tagId }),
+      ).catch((err) => {
+        console.error('ERR', err);
+        throw err;
+      });
+    },
+  ).then(() => process.exit());
+
+const createTables = async () => db.query(schemaSql);
+
+const doPopulate = async () => {
+  console.log('creating tables');
+  await createTables();
+  console.log('injecting data');
+  await populatePhotos();
+};
+
+dbPopulate();
